c7a09f6cb542bed08613378ff35914453921d413
Evax Software <contact@evax.fr>
2013-02-18 09:48:03 +0100

    Add atoms support to coerce_c_string

--- src/xmerlsec_util.erl
+++ src/xmerlsec_util.erl
@@ -1,6 +1,8 @@
 -module(xmerlsec_util).
 -export([coerce_c_string/1]).
 
+coerce_c_string(S) when is_atom(S) ->
+    coerce_c_string(atom_to_list(S));
 coerce_c_string(S) when is_list(S) ->
     coerce_c_string(list_to_binary(S));
 coerce_c_string(S) ->
